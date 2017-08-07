4df66f6eb170355a78fe63e7789503a1fe991a91
jstarling1 <jennstarling@gmail.com>
2017-08-07 17:51:16 -0500

    Starling Folder Update

--- R/setupProject.R
+++ R/setupProject.R
@@ -55,7 +55,6 @@ setUpProject <- function(folderStyle="JenniferStarling",
                        'R','R/Cache','R/Output','R/Figs','R/Reports',
                        'Paper','Paper/Inputs','Paper/Proofs','Paper/Final',
                        'Paper/Reviews','Paper/Proofs','Paper/Submitted',
-                       'Paper/Inputs/Output','Paper/Inputs/Figs','Paper/Inputs/Reports',
                        'Reference','Notes','ProjectInfo')
      
      projectInfo$folderStyle <- folderStyle
