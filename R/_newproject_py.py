70ffee71c062f1627a02fd3fe1cb7848e6781ddb
jestarling <jennstarling@gmail.com>
2017-09-08 14:17:50 -0500

    Update py proj

---
+++ R/_newproject_py.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+#===============================================================
+# User Inputs
+#===============================================================
+
+# Set path of directory for root of project.
+myPath = '/Users/jennstarling/Stats/Projects/learnPython/TestProject'
+
+
+#===============================================================
+# Set up new folders.
+#===============================================================
+
+# Sets up file system in directory where this file is located.
+import os
+os.chdir(myPath)
+os.getcwd()
+
+# Set up new folders. 
+
+newFolders = ['Data/', 'DataRaw/', 
+              'FromCollaborator/',
+              'Py/','Py/Cache/','Py/Output/','Py/Figs/','Py/Reports', 'Py/Tables/',
+              'Paper/','Paper/Inputs/','Paper/Proofs/',
+              'Paper/Final/', 'Paper/Submitted/',
+              'Reference/', 'Notes/', 'ProjectInfo/'
+              ]   
+
+for i in range(0,len(newFolders)):
+    os.makedirs(os.path.join(myPath,newFolders[i]))         
+
+# Add .gitkeep files to all directories so git does not drop empty directories.
+x = 'find . -type d -empty -not -path "./.git/*" -exec touch {}/.gitkeep \;'
+os.system(x)
+
+
+
